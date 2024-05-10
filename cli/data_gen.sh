9d12ecac9fb7a7b348b7d8e19933d314e70ee758
Nikita Janakarajan <nja@zurich.ibm.com>
2024-05-10 12:26:10 +0200

    ruff formatted

--- cli/data_gen.sh
+++ cli/data_gen.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 TARGET=${1:-"cms"}
-CV_SPLITS_PATH=${2:-"/Users/nja/Desktop/Sinergia/data/tinder_expts/5x5stratified_v2_check/cv_splits.json"}
-OUTPUT_DIR=${3:-"/Users/nja/Desktop/Sinergia/data/tinder_expts/5x5stratified_v2_check"}
+CV_SPLITS_PATH=${2:-"/Users/nja/Desktop/Sinergia/data/tinder_expts/5x5stratified_20percent/cv_splits_strat.json"}
+OUTPUT_DIR=${3:-"/Users/nja/Desktop/Sinergia/data/tinder_expts/5x5stratified_20percent"}
 PROBEMAP_PATH=${4:-"/Users/nja/Library/CloudStorage/Box-Box/Molecular_SysBio/projects/Sinergia_CRC/data/gdc_coad/gene_expression/gencode.v22.annotation.gene.probeMap"}
 COLOTYPE_PATH=${5:-"/Users/nja/Desktop/Sinergia/data/colotype_gex/colotype_genes.csv"}
-for sampling_method in unaugmented gamma_poisson poisson local_crossover global_crossover smote replacement
+for sampling_method in gamma_poisson poisson local_crossover global_crossover smote replacement
     do
         for class_size in max 500 5000
             do
         
                 python /Users/nja/public_git/signature_sampling/scripts/data_gen.py \
                 --cv_splits_path $CV_SPLITS_PATH \
-                --ref_df_path $OUTPUT_DIR/"tcga_colotype_gex_counts.csv" \
-                --ref_labels_path $OUTPUT_DIR/"tcga_colotype_gex_labels.csv"\
+                --ref_df_path "/Users/nja/Desktop/Sinergia/data/tinder_expts/tcga_colotype_gex_counts.csv" \
+                --ref_labels_path "/Users/nja/Desktop/Sinergia/data/tinder_expts/tcga_colotype_gex_labels.csv"\
                 --probemap_path $PROBEMAP_PATH \
                 --colotype_path $COLOTYPE_PATH \
                 --sampling_method $sampling_method \
@@ -25,6 +25,25 @@ for sampling_method in unaugmented gamma_poisson poisson local_crossover global_
             done 
     done
 
+for sampling_method in unaugmented
+    do
+        for class_size in None
+            do
+        
+                python /Users/nja/public_git/signature_sampling/scripts/data_gen.py \
+                --cv_splits_path $CV_SPLITS_PATH \
+                --ref_df_path "/Users/nja/Desktop/Sinergia/data/tinder_expts/tcga_colotype_gex_counts.csv" \
+                --ref_labels_path "/Users/nja/Desktop/Sinergia/data/tinder_expts/tcga_colotype_gex_labels.csv"\
+                --probemap_path $PROBEMAP_PATH \
+                --colotype_path $COLOTYPE_PATH \
+                --sampling_method $sampling_method \
+                --class_size $class_size \
+                --target $TARGET \
+                --validation_size 0.1 \
+                --seed 9 \
+                --save_dir $OUTPUT_DIR/$sampling_method/$class_size
+            done 
+    done
 
 
 
