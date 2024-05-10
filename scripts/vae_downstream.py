ae4a61112b2c52e41c6614ce992394c32799f253
Nikita Janakarajan <nja@zurich.ibm.com>
2024-05-10 12:24:19 +0200

    fix: added main to run from cli, fixed redundant code

--- scripts/vae_downstream.py
+++ scripts/vae_downstream.py
@@ -1,13 +1,16 @@
 import os
-import argparse
+from pathlib import Path
+from typing import Counter, Dict, Iterable
+
+import click
+import joblib
 import numpy as np
 import pandas as pd
 import torch
-from sklearn.metrics import roc_auc_score, balanced_accuracy_score
-import joblib
-from typing import Counter, Iterable, Dict
-from signature_sampling.utils import load_model, get_latent_embeddings
+from sklearn.metrics import balanced_accuracy_score, roc_auc_score
+
 from signature_sampling.hyperparameter_factory import CLASSIFIER_FACTORY
+from signature_sampling.utils import get_latent_embeddings, load_model
 
 
 def predict_clinical(
@@ -53,33 +56,31 @@ def predict_clinical(
     os.makedirs(os.path.join(result_dir, "summary_results"), exist_ok=True)
     mean_acc = []
     std_acc = []
+    mean_auc = []
+    std_auc = []
     cptac_mean_acc = []
     cptac_std = []
+    cptac_mean_auc = []
+    cptac_std_auc = []
     size_copy = size
     mean_metrics = pd.DataFrame(
-        columns=["bal_acc", "cptac_bal_acc"],
+        columns=["bal_acc", "std_bal_acc", "cptac_bal_acc", "std_cptac_bal_acc"],
         index=sampling_methods,
     )
-    std_metrics = pd.DataFrame(
-        columns=["bal_acc", "cptac_bal_acc"], index=sampling_methods
-    )
+    # std_metrics = pd.DataFrame(
+    #     columns=["bal_acc", "cptac_bal_acc"], index=sampling_methods
+    # )
 
     for sampling in sampling_methods:
         scores = []
         acc = []
-
         cptac_acc = []
+        cptac_scores = []
+
         metrics = pd.DataFrame(columns=["bal_acc", "cptac_bal_acc"])
 
         if sampling == "unbalanced":
             size = ""
-
-        save_dir = os.path.join(result_dir, f"{sampling}", size)
-
-        scores = []
-        acc = []
-
-        metrics = pd.DataFrame(columns=["bal_acc"])
 
         save_dir = os.path.join(result_dir, f"{sampling}", size)
 
@@ -190,8 +191,16 @@ def predict_clinical(
 
             acc.append(bal_acc)
 
+            auc = roc_auc_score(
+                test_target,
+                estimator.predict_proba(test_latent.detach().numpy()),
+                multi_class="ovr",
+            )
+            scores.append(auc)
+
             if external:
                 cptac_preds = estimator.predict(external_latent.detach().numpy())
+                cptac_prob = estimator.predict_proba(external_latent.detach().numpy())
 
                 # CPTAC
                 drop_idx = np.argwhere(pd.isna(external_target[clinical_var].values))
@@ -203,26 +212,134 @@ def predict_clinical(
                 )
                 cptac_acc.append(cptac_bal_acc)
 
+                cptac_auc = roc_auc_score(
+                    external_target[clinical_var][keep_idx],
+                    cptac_prob[keep_idx],
+                    multi_class="ovr",
+                )
+                cptac_scores.append(cptac_auc)
+
         metrics["bal_acc"] = acc
+        metrics["auc_roc"] = scores
         if external:
             metrics["cptac_bal_acc"] = cptac_acc
             cptac_mean_acc.append(np.mean(cptac_acc))
             cptac_std.append(np.std(cptac_acc))
+            cptac_mean_auc.append(np.mean(cptac_auc))
+            cptac_std_auc.append(np.std(cptac_auc))
 
         metrics.to_csv(os.path.join(result_dir, f"{sampling}", size, "metrics.csv"))
 
         mean_acc.append(np.mean(acc))
         std_acc.append(np.std(acc))
+        mean_auc.append(np.mean(auc))
+        std_auc.append(np.std(auc))
 
     mean_metrics["bal_acc"] = mean_acc
-    std_metrics["bal_acc"] = std_acc
+    mean_metrics["std_bal_acc"] = std_acc
+    mean_metrics["roc_auc"] = mean_auc
+    mean_metrics["std_roc_auc"] = std_auc
+
     if external:
         mean_metrics["cptac_bal_acc"] = cptac_mean_acc
-        std_metrics["cptac_bal_acc"] = cptac_std
+        mean_metrics["std_cptac_bal_acc"] = cptac_std
+        mean_metrics["cptac_roc_auc"] = cptac_mean_auc
+        mean_metrics["std_cptac_roc_auc"] = cptac_std_auc
 
     mean_metrics.to_csv(
         os.path.join(result_dir, f"summary_results/mean_metrics_{size_copy}.csv")
     )
-    std_metrics.to_csv(
-        os.path.join(result_dir, f"summary_results/std_metrics_{size_copy}.csv")
-    )
+
+
+@click.command()
+@click.option(
+    "--data_dir",
+    required=True,
+    type=click.Path(path_type=Path, exists=True),
+    help="Root directory of datasets.",
+)
+@click.option(
+    "--model_dir",
+    required=True,
+    type=click.Path(path_type=Path, exists=True),
+    help="Directory where the VAE model is saved.",
+)
+@click.option(
+    "--result_root_dir",
+    required=True,
+    type=click.Path(path_type=Path, exists=True),
+    help="Directory where results should be saved.",
+)
+@click.option(
+    "--cv_splits_path",
+    required=True,
+    type=click.Path(path_type=Path, exists=True),
+    help="Path to the CV splits json file.",
+)
+@click.option(
+    "--clinical_data_path",
+    required=True,
+    type=click.Path(path_type=Path, exists=True),
+    help="Path to the clinical data csv.",
+)
+@click.option(
+    "--clinical_var", type=str, help="Clinical variable name chosen for classification."
+)
+@click.option(
+    "--external_clinical_path",
+    required=True,
+    type=click.Path(path_type=Path, exists=True),
+    help="Path to external data's clinical csv file",
+)
+@click.option("--seed", type=int, help="Seed for reproducibility.")
+def main(
+    data_dir,
+    model_dir,
+    result_root_dir,
+    cv_splits_path,
+    clinical_data_path,
+    clinical_var,
+    external_clinical_path,
+    seed,
+):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    vae_model_name = "NVAE"
+
+    with open(cv_splits_path, "r") as f:
+        cv_splits_idx = json.load(f)
+
+    sampling_methods = [
+        "poisson",
+        "gamma_poisson",
+        "local_crossover",
+        "global_crossover",
+        "smote",
+        "replacement",
+        "unaugmented",
+    ]
+
+    clinical_data = pd.read_csv(clinical_data_path, index_col=0)
+    external_clinical_df = pd.read_csv(external_clinical_path, index_col=0)
+
+    for clf_model in ["MLP", "RF", "Logistic", "SVM-RBF", "KNN", "EBM"]:
+        for size in ["max", "500", "5000"]:
+            predict_clinical(
+                clf_model,
+                data_dir,
+                model_dir,
+                vae_model_name,
+                result_root_dir,
+                sampling_methods,
+                size,
+                cv_splits_idx,
+                clinical_data,
+                clinical_var,
+                external=True,
+                external_clinical=external_clinical_df,
+                n_splits=25,
+            )
+
+
+if __name__ == "__main__":
+    main()
